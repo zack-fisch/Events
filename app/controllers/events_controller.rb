f55cfacbc4acf60996ab8aa3c398f10f0032842a
Zack Fisch <zack@zfisch.com>
2014-07-18 08:49:26 -0400

    Implementing event updating/editing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,4 +12,11 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+	def update
+		@event = Event.find(params[:id])
+		event_params = params.require(:event).
+				permit(:name, :description, :location, :price, :starts_at)
+		@event.update(event_params)
+		redirect_to @event
+	end
 end
