7dcdb90cfed397b942b628aa6310f79acb2adc49
Zack Fisch <zack@zfisch.com>
2014-07-20 13:18:29 -0400

    Adding images to events :raised_hands:

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,12 @@ module EventsHelper
 			number_to_currency(event.price)
 		end
 	end
+
+	def image_for(event)
+		if event.image_file_name.blank?
+			image_tag 'placeholder.png', size: "200"
+		else
+			image_tag event.image_file_name, size: "200"
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715233540) do
+ActiveRecord::Schema.define(version: 20140720165043) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140715233540) do
     t.datetime "updated_at"
     t.datetime "starts_at"
     t.text     "description"
+    t.string   "image_file_name"
+    t.integer  "capacity",        default: 1
   end
 
 end
